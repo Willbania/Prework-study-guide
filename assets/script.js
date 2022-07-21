e46b76fedbf7662c2865636f458b358eb242b5cb
William Formato <formatowilliam@gmail.com>
2022-07-21 17:25:39 +0930

    added javascript

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+var topics = ['html', 'CSS', 'Git', 'JavaScript'];
+
+
+for(var x = 0; x < topics.length; x++){
+
+    console.log(topics[x]);
+
+
+}
+
+/*
+if (topic === 'HTML') {
+
+    console.log("Let's study HTMML!");
+} else if (topic === 'CSS') {
+
+console.log("let's study 'CSS!'");
+
+} else if (topic === 'Git') {
+
+    console.log("let's study Git!");
+
+} else if (topic === 'JavaScript') {
+
+console.log("lets study JavaScript!");
+
+} else {
+
+console.log('please try again!');
+
+}
+
+
+var shapes = ["triangle", "square", "pentagon", "circle"]
+
+
+
+            for(var x = 0; x < shapes.length; x++){
+
+        console.log(shapes[x]);
+
+            }
+
+*/
